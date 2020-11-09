cc5afb8fd734a5345c5f7cfab9c08e16437990dd
Matteo Mancini <ingmatteomancini@gmail.com>
2020-11-09 17:32:22 +0000

    Fixed path issues.

--- tool.py
+++ tool.py
@@ -8,7 +8,6 @@ from dipy.io.streamline import load_tractogram
 
 import shutil
 import scipy.ndimage.morphology
-from dipy.io.gradients import read_bvals_bvecs
 from dipy.tracking import utils
 from dipy.tracking.streamlinespeed import length
 
@@ -46,22 +45,26 @@ def run(context):
     #############################
     # Fitting NODDI using AMICO #
     #############################
+
     amico.core.setup()
 
-    ae = amico.Evaluation("/root/", ".")
+    ae = amico.Evaluation('/root/', '.')
 
-    [_, bvecs] = read_bvals_bvecs(None, hcpl_bvalues_file_path)
-    bvecs_norm = normalized_vector(hcpl_bvecs_file_path)
+    [_, bvecs] = read_bvals_bvecs(None, hcpl_bvecs_file_path)
+    bvecs_norm = normalized_vector(bvecs)
     bvecs_norm[0] = [0, 0, 0]
     np.savetxt('/root/grad_norm.txt', np.matrix.transpose(bvecs_norm), fmt='%.3f')
 
-    amico.util.fsl2scheme(hcpl_bvalues_file_path, '/root/grad_norm.txt')
+    amico.util.fsl2scheme(hcpl_bvalues_file_path, '/root/grad_norm.txt', '/root/grad.scheme')
 
-    ae.load_data(dwi_filename="prep.dwi.hcpl.nii.gz",
-                 scheme_filename="bval.scheme", mask_filename="mask.nii.gz", b0_thr=30)
+    os.system('/root/mrtrix3/bin/dwi2mask -fslgrad ' + hcpl_bvecs_file_path + ' ' +
+              hcpl_bvalues_file_path+' '+hcpl_dwi_file_path+' /root/mask.nii.gz')
 
-    ae.set_model("NODDI")
-    ae.generate_kernels(regenerate=True)
+    ae.load_data(dwi_filename='prep.dwi.hcpl.nii.gz',
+                 scheme_filename='grad.scheme', mask_filename='mask.nii.gz', b0_thr=30)
+
+    ae.set_model('NODDI')
+    ae.generate_kernels()
     ae.load_kernels()
 
     ae.fit()
@@ -72,21 +75,23 @@ def run(context):
     # Computing inclusion/exclusion maps from NODDI maps #
     ######################################################
 
-    os.system('mrcalc /root/AMICO/NODDI/FIT_OD.nii.gz 0.1 -gt ' +
+    os.system('/root/mrtrix3/bin/mrcalc /root/AMICO/NODDI/FIT_OD.nii.gz 0.1 -gt ' +
               '/root/AMICO/NODDI/FIT_OD.nii.gz 0.7 -lt -mul /root/wm_mask.nii.gz')
 
-    os.system('mrcalc /root/AMICO/NODDI/FIT_ICVF.nii.gz 0.95 -lt /root/gm_mask.nii.gz')
+    os.system('/root/mrtrix3/bin/mrcalc /root/AMICO/NODDI/FIT_ICVF.nii.gz 0.95 -lt /root/gm_mask.nii.gz')
 
-    os.system('mrcalc /root/AMICO/NODDI/FIT_ISOVF.nii.gz 0 -gt /root/csf_mask.nii.gz')
+    os.system('/root/mrtrix3/bin/mrcalc /root/AMICO/NODDI/FIT_ISOVF.nii.gz 0 -gt /root/csf_mask.nii.gz')
 
     ##################################################
     # Doing reconstruction&tracking using TRAMPOLINO #
     ##################################################
+
     os.chdir('/root')
-    os.system('trampolino -r results -n mrtrix_workflow recon -i prep.dwi.hcpl.nii.gz ' +
-              '-v prep.gradients.hcpl.txt -b prep.bvalues.hcpl.txt ' +
+    os.system('trampolino -r results -n mrtrix_workflow recon -i ' + hcpl_dwi_file_path + ' ' +
+              '-v ' + hcpl_bvecs_file_path + ' -b ' + hcpl_bvalues_file_path + ' ' +
               '--opt bthres:0,mask:wm_mask.nii.gz mrtrix_msmt_csd track' +
-              '-s prep.inject.nii.gz --opt nos:10000,include:gm_mask.nii.gz,exclude:csf_mask.nii.gz ' +
+              '-s ' + inject_file_path +
+              ' --opt nos:10000,include:gm_mask.nii.gz,exclude:csf_mask.nii.gz ' +
               '--min_length 10,50 --ensemble min_length mrtrix_tckgen ' +
               'convert -r wm_mask.nii.gz tck2trk')
 
@@ -103,15 +108,16 @@ def run(context):
     # For VUMC, additional streamline filtering is done using anatomical      #
     # priors (keeping only streamlines that intersect with at least one ROI). #
     ###########################################################################
-    if postprocessing in ["EPFL", "ALL"]:
+
+    if postprocessing in ['EPFL', 'ALL']:
         context.set_progress(message='Processing density map (EPFL)')
-        volume_folder = "/root/vol_epfl"
-        output_epfl_zip_file_path = "/root/TrackyMcTrackface_EPFL_example.zip"
+        volume_folder = '/root/vol_epfl'
+        output_epfl_zip_file_path = '/root/SpaghettiBeans_EPFL.zip'
         os.mkdir(volume_folder)
         lengths = length(streamlines)
         streamlines = streamlines[lengths > 1]
         density = utils.density_map(streamlines, affine, seed_mask_img.shape)
-        density = scipy.ndimage.gaussian_filter(density.astype("float32"), 0.5)
+        density = scipy.ndimage.gaussian_filter(density.astype('float32'), 0.5)
 
         log_density = np.log10(density + 1)
         max_density = np.max(log_density)
@@ -120,16 +126,16 @@ def run(context):
             nbr = nbr.zfill(3)
             mask = log_density >= t
             vol_filename = os.path.join(volume_folder,
-                                        "vol" + nbr + "_t" + str(t) + ".nii.gz")
-            nib.Nifti1Image(mask.astype("int32"), affine,
+                                        'vol' + nbr + '_t' + str(t) + '.nii.gz')
+            nib.Nifti1Image(mask.astype('int32'), affine,
                             seed_mask_img.header).to_filename(vol_filename)
         shutil.make_archive(output_epfl_zip_file_path[:-4], 'zip', volume_folder)
 
-    if postprocessing in ["VUMC", "ALL"]:
+    if postprocessing in ['VUMC', 'ALL']:
         context.set_progress(message='Processing density map (VUMC)')
         ROIs_img = nib.load(VUMC_ROIs_file_path)
-        volume_folder = "/root/vol_vumc"
-        output_vumc_zip_file_path = "/root/TrackyMcTrackface_VUMC_example.zip"
+        volume_folder = '/root/vol_vumc'
+        output_vumc_zip_file_path = '/root/SpaghettiBeans_VUMC.zip'
         os.mkdir(volume_folder)
         lengths = length(streamlines)
         streamlines = streamlines[lengths > 1]
@@ -142,7 +148,7 @@ def run(context):
         streamlines = streamlines[grouping[(0, 1)]]
 
         density = utils.density_map(streamlines, affine, seed_mask_img.shape)
-        density = scipy.ndimage.gaussian_filter(density.astype("float32"), 0.5)
+        density = scipy.ndimage.gaussian_filter(density.astype('float32'), 0.5)
 
         log_density = np.log10(density + 1)
         max_density = np.max(log_density)
@@ -151,18 +157,19 @@ def run(context):
             nbr = nbr.zfill(3)
             mask = log_density >= t
             vol_filename = os.path.join(volume_folder,
-                                        "vol" + nbr + "_t" + str(t) + ".nii.gz")
-            nib.Nifti1Image(mask.astype("int32"), affine,
+                                        'vol' + nbr + '_t' + str(t) + '.nii.gz')
+            nib.Nifti1Image(mask.astype('int32'), affine,
                             seed_mask_img.header).to_filename(vol_filename)
         shutil.make_archive(output_vumc_zip_file_path[:-4], 'zip', volume_folder)
 
     ###################
     # Upload the data #
     ###################
+
     context.set_progress(message='Uploading results...')
-    if postprocessing in ["EPFL", "ALL"]:
+    if postprocessing in ['EPFL', 'ALL']:
         context.upload_file(output_epfl_zip_file_path,
                             'SpaghettiBeans_EPFL.zip')
-    if postprocessing in ["VUMC", "ALL"]:
+    if postprocessing in ['VUMC', 'ALL']:
         context.upload_file(output_vumc_zip_file_path,
                             'SpaghettiBeans_VUMC.zip')
